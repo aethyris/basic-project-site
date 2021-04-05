b34f8c58c9563af965d61e426184542e5813a201
Calvin Kwong <aethyrisx@gmail.com>
2021-04-05 19:17:36 -0400

    Changed the name prompt and quiz selection to two separate views

--- Assignment03/js/quiz.js
+++ Assignment03/js/quiz.js
@@ -1,14 +1,40 @@
+const source = 'https://my-json-server.typicode.com/aethyris/cus1172-project1/';
+let studentName = '';
+let quizSelector = 'quiz1';
+
 document.addEventListener('DOMContentLoaded', () => {
-    renderView('#quiz-selection');
+    renderView('#provide-name');
+    // createQuestionView(1);
 })
 
-const renderQuestionView = async (questionID) => {
-    const data = await fetch(`https://`)
-}
-
-const renderView = (view) => {
+const renderView = (view, model) => {
     const templateSource = document.querySelector(view).innerHTML;
     const template = Handlebars.compile(templateSource);
-    const html = template();
+    const html = template(model);
     document.querySelector('#app-widget').innerHTML = html;
+}
+
+const selectQuizView = async () => {
+    studentName = document.querySelector('#student-name').value;
+    if (studentName == '') {
+        alert('Please enter a valid name.')
+    } else {
+        const data = await fetch(`${source}/quiz1`);
+        const dataJSON = await data.json();
+        console.log(dataJSON);
+        const model = {
+            'name': studentName,
+        };
+        renderView('#select-quiz', model);
+    }
+}
+
+const createQuestionView = async (questionId) => {
+    const data = await fetch(`${source}${quizSelector}/?id=${questionId}`);
+    const model = await data.json();
+    switch(model[0].type) {
+        case 'multiple-choice':
+            renderView('#multiple-choice', model[0]);
+            break;
+    }
 }
